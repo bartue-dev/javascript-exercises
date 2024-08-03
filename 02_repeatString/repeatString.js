cb264a9ae8d207557c2693984033e9515aafbba6
Bartue <[bartbatiancila28@gmail.com]>
2024-08-03 12:06:00 +0800

    Update the function with two arguments string it should return the string many times base of what is the value of the num arguments. shouldnt loop if the num value is less than 0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let stringValue = string;
+  const numValue = num;
+  let res = '';
 
+  if(numValue < 0){
+    res += stringValue.replace("goodbye", "ERROR")
+  }
+  for (i = 0; i < numValue; i++){
+      res += stringValue;
+  }
+  return res;
 };
 
 // Do not edit below this line
