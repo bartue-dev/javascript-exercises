51a11dc1835698f6275057e03b453c248e6a2f18
Bartue <[bartbatiancila28@gmail.com]>
2024-08-03 21:30:00 +0800

    Add two arguments start and end if start and end is not a number or its have a negative value returns ERROR. After that loop thorugh two arguments as indexes then sum up all indexes using reduce method

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+  let res = []; 
 
+  if(!Number.isInteger(start) || !Number.isInteger(end)){
+    return "ERROR"
+  } else if (start < 0 || end < 0) {
+    return "ERROR"
+  } else if (typeof start !== "number" || typeof end !== "number") {
+    return "ERROR"
+  }
+  for(i = start; i <= end; i++){
+    res.push(i);
+  }
+  for(j = start; j >= end; j--){
+    res.push(j);
+  }
+  const sum = res.reduce((acc,curr) => acc + curr, 0);
+  return sum;
 };
 
 // Do not edit below this line
