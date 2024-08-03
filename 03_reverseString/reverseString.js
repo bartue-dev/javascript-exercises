0bd5fa23f0928775e57310bf3998571dc7d0c621
Bartue <[bartbatiancila28@gmail.com]>
2024-08-03 12:52:27 +0800

    Add str argument in function if str is empty string or blank it should return empty string the reverse the string using reduce method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  if(str === ""){
+    return "";
+  }
+  const reverse = str.split("").reduce((acc, char) => {
+    return char + acc;
+  });
+  return reverse;
 };
 
 // Do not edit below this line
