317c266eb12521102f5167c5585bcdb2095f9ddc
Bartue <[bartbatiancila28@gmail.com]>
2024-08-04 21:14:24 +0800

    Add one parameter/argument to pass the value of year, add if else statement, if year is divisible by 4 and year is not divisible by 100 or year is divisible by 100 and 400 return true otherwise false

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+const four = year % 4 === 0;
+const notOneHundred = year % 100 !== 0;
+const oneHundred = year % 100 === 0;
+const fourHundred = year % 400 === 0;
+
+if(four && notOneHundred || oneHundred && fourHundred){
+  return true;
+}else {
+  return false
+}
 };
 
 // Do not edit below this line
