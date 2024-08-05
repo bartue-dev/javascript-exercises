1e3eb1c7055bdc51a650b228fe8401aed586c5f5
Bartue <[bartbatiancila28@gmail.com]>
2024-08-05 19:11:08 +0800

    Add diffent statements inside each function base of what is to return. Addition subtraction sum multiply power and factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(firstValue, secondValue) {
+	return firstValue + secondValue;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstValue, secondValue) {
+  return firstValue - secondValue;
 };
 
-const sum = function() {
-	
+const sum = function(integers) {
+  let sum = 0;
+  for(i = 0; i < integers.length; i++){
+    sum += integers[i];
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(integers) {
+  let result = 1;
+  for(i = 0; i < integers.length; i++){
+    result *= integers[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(firstValue, secondValue) {
+	return Math.pow(firstValue, secondValue)
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+	let result = 1;
+  for(i = factorial; i > 0; i--){
+    if(!factorial){
+      result;
+    }else{
+      result *= i;
+    }
+  }
+  return result
 };
 
 // Do not edit below this line
