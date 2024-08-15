74ef6654a3685efc66941a7dc200d63cb0365803
Bartue <[bartbatiancila28@gmail.com]>
2024-08-15 22:57:43 +0800

    Add new property name age. This age is the difference of year of death and year of birth but if no year of death use current date.Use sort method to display using the age property so it would display the oldest at the top. Return the first element of the array since it is sorted decending.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(oldest) {
+  const date = new Date().getFullYear();
+  oldest.map((item) => {
+    const age = item.yearOfDeath - item.yearOfBirth || date - item.yearOfBirth;
+    return item.age = age;
+  });
 
+  oldest.sort((a,b) => {
+    if(a.age > b.age){
+      return -1
+    }else {
+      return 1
+    }
+  })
+
+  return oldest[0]
+  
 };
 
 // Do not edit below this line
