f3d900323bd24bfaa89ea08af264b67feb058f8b
Bartue <[bartbatiancila28@gmail.com]>
2024-08-15 21:12:16 +0800

    Add fib variable and initiallize it with array 0 and 1. First convert the arg to Number. Check the arg if 0,1 and negative number if its true return 0,1,and OOPS. Use for loop to get the member of the fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  let mem = Number(member)
+  let fib = [0,1];
+  let res;
 
+  if(mem === 0 ){
+    res = 0
+  }
+  if(mem === 1){
+    res = 1
+  }
+  if(mem < 0){
+    res = "OOPS"
+  }
+  for(i = 2; i <= mem; i++){
+    fib[i] = fib[i - 2] + fib[i - 1];
+    res = fib[i]
+  }
+  return res
 };
 
 // Do not edit below this line
